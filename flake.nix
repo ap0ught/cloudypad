4b704b919254b82397fca7b367cad36839050d24
Christopher Mayfield <cdm@ap0.org>
2025-08-13 15:53:53 -0500

    chore: prepare release 0.35.1 - update version in package files and scripts

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let  
         pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
-        cloudypadVersion = "0.35.0";
+        cloudypadVersion = "0.35.1";
       in {
         packages = rec {
           default = cloudypad;
@@ -18,7 +18,7 @@
 
             src = pkgs.fetchurl {
               url = "https://raw.githubusercontent.com/ap0ught/cloudypad/v${cloudypadVersion}/cloudypad.sh";
-              hash = "sha256:1l400pwiaw721s0zscanvwiypxj90i01sp42s2bp1yhg3226l5rj";
+              hash = "sha256-QLPScRCNcOKmq5sOPdM/ZX1Sh782eH+ijUx27qHajXQ=";
             };
 
             phases = [ "installPhase" ];
